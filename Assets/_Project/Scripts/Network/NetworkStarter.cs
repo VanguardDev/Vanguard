21f3c9c67ad1d9a0cbdd2214eb5c17e40eec744a
Keith Bartlett <kieth.tienyou.bartlett@gmail.com>
2021-11-13 14:15:50 -0800

    networkautostart

--- Assets/_Project/Scripts/Network/NetworkStarter.cs
+++ Assets/_Project/Scripts/Network/NetworkStarter.cs
@@ -8,23 +8,26 @@ public class NetworkStarter : MonoBehaviour
     NodeListServer.NodeListServerNetManager nm;
     public void Start()
     {
-        if (!ConnectionInfo.isOpenedFromMenu)
-        {
-            FindObjectOfType<NetworkManagerHUD>().enabled = true;
-            return;
-        }
-        string externalIP = new WebClient().DownloadString("http://icanhazip.com").Trim('\n'); // could be the cause of process leak ?
-        if (Application.isBatchMode) return;
-        nm = GetComponent<NodeListServer.NodeListServerNetManager>();
-        if (ConnectionInfo.Mode == 1) nm.StartHost();
-        else if (ConnectionInfo.Mode == 2) nm.StartServer();
-        else
-        {
-            if (externalIP == ConnectionInfo.ip) ConnectionInfo.ip = "localhost";
-            Debug.Log(ConnectionInfo.ip == externalIP);
-            Debug.Log(externalIP);
-            nm.networkAddress = ConnectionInfo.ip;
-            nm.StartClient();
-        }
+        var manager = GetComponent<NetworkManager>();
+        manager.StartHost();
+        return;
+        // if (!ConnectionInfo.isOpenedFromMenu)
+        // {
+        //     FindObjectOfType<NetworkManagerHUD>().enabled = true;
+        //     return;
+        // }
+        // string externalIP = new WebClient().DownloadString("http://icanhazip.com").Trim('\n'); // could be the cause of process leak ?
+        // if (Application.isBatchMode) return;
+        // nm = GetComponent<NodeListServer.NodeListServerNetManager>();
+        // if (ConnectionInfo.Mode == 1) nm.StartHost();
+        // else if (ConnectionInfo.Mode == 2) nm.StartServer();
+        // else
+        // {
+        //     if (externalIP == ConnectionInfo.ip) ConnectionInfo.ip = "localhost";
+        //     Debug.Log(ConnectionInfo.ip == externalIP);
+        //     Debug.Log(externalIP);
+        //     nm.networkAddress = ConnectionInfo.ip;
+        //     nm.StartClient();
+        // }
     }
 }
